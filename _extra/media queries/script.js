c84df141cf45ed2193def93c8908c18743f56ed3
Zpulley <77287249+Zpulley@users.noreply.github.com>
2022-11-11 01:30:51 -0500

    1

---
+++ _extra/media queries/script.js
@@ -0,0 +1,14 @@
+//let test1 = "why is this not working?";
+
+//let intl =5;
+
+//let float1= true;
+
+//console.log(test1);
+
+//let container = documentSelector(".grid1");
+function lol(){
+let box = documentSelector(".container");
+box.classList.toggle("newClass1");
+
+}
